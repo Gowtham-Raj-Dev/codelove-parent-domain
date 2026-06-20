6467b08d5c5461dde874af1bdfd21370af4cd945
Gowtham <gowthamraje1221@gmail.com>
2026-06-20 17:11:45 +0530

    fiexed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubActions ? "/codelove-parent-domain" : "",
   images: {
     unoptimized: true,
   },
